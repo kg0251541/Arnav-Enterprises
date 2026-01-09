e6b4f85c5d80348dbdaf95c0d533abb8bd93ad74
KUNAL GUPTA <kg0251541@gmail.com>
2026-01-09 11:46:21 +0530

    Update Products.jsx

--- frontend/src/components/Products.jsx
+++ frontend/src/components/Products.jsx
@@ -23,7 +23,7 @@ export default function Products() {
 
   const fetchProducts = async () => {
     try {
-      const res = await axios.get("http://localhost:5000/api/products");
+      const res = await axios.get("https://bakend-p4gh.onrender.com/api/products");
       setProducts(res.data);
     } catch (err) {
       console.error(err);
